1f8d3c2af09dc4817b63078650528e46ff4ec0d5
Vladimir Lazarenko <favoretti@gmail.com>
2020-05-08 11:03:30 +0200

    Added tests

--- TerraformCLI/src/tests/terraform-refresh/refresh-with-secure-env-file.ts
+++ TerraformCLI/src/tests/terraform-refresh/refresh-with-secure-env-file.ts
@@ -4,8 +4,10 @@ import '../scenarios-terraform';
 import { env } from './refresh-with-secure-env-file.env';
 
 export let refreshWithNoArgs = new TaskScenario<TerraformInputs>()
+    .inputAzureRmServiceEndpoint(env.environmentServiceName, env.subscriptionId, env.tenantId, env.servicePrincipalId, env.servicePrincipalKey)
     .inputTerraformCommand(env.terraformCommand)
     .inputTerraformSecureVarsFile(env.secureVarsFileId, env.secureVarsFileName)
+    .input({ environmentServiceName: env.environmentServiceName })
     .inputApplicationInsightsInstrumentationKey()    
     .answerTerraformExists()
     .answerTerraformCommandIsSuccessful()
